1229dfb2a44b2a4712d12bbda7c7a5d02b002d14
jazminh4308 <jazmin.h4308@gmail.com>
2026-02-09 12:47:27 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by HERNANDEZ CAZARES, JAZMIN on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
