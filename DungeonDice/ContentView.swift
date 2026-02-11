11c5a9ca9b6503bafe1b07f655f559dfdecc0497
jazminh4308 <jazmin.h4308@gmail.com>
2026-02-11 13:09:31 -0800

    Added to xcode ( feb 11th lesson videos)

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -7,7 +7,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice: Int, CaseIterable {
+    enum Dice: Int, CaseIterable, Identifiable {
+        
         case four = 4
         case six = 6
         case eight = 8
@@ -16,12 +17,20 @@ struct ContentView: View {
         case twenty = 20
         case hundred = 100
         
+        var id: Int { //one value computed properties (or functions) don't need return
+             rawValue //Each rawValue is unique, so it's a good ID
+        }
+        
+        var description: String { "\(rawValue)-sided" }
+        
         func roll() -> Int {
             return Int.random(in: 1...self.rawValue)
         }
     }
     
     @State private var resultMessage = ""
+    @State private var animationTrigger = false // changed when animation occured
+    @State private var isDoneAnimating = true
     
     var body: some View {
         VStack {
@@ -36,18 +45,26 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
+                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1, y: 0, z: 0))
                 .frame(height: 150)
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false //set to beginning "false" state right away
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                        isDoneAnimating = true
+                    }
+                }
             
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                ForEach(Dice.allCases, id: \.self) { dice in
-                    Button("\(dice.rawValue)-sided") {
-                        resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                ForEach(Dice.allCases) { dice in
+                    Button(dice.description) {
+                        resultMessage = "You rolled a \(dice.roll()) on a \(dice)-sided dice"
+                        animationTrigger.toggle() // a change of this value triggers an animation
                     }
-            }
-            .buttonStyle(.borderedProminent)
-            .tint(.red)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
             }
         }
         .padding()
